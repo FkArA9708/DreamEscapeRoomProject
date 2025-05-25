c1b39fffce68d724f8b3064e72e678e441c7a627
9007365FkArA <9007365@student.zadkine.nl>
2025-05-25 13:48:37 +0200

    klaar

---
+++ lostescaperoom.php
@@ -0,0 +1,26 @@
+<?php require_once 'dbcon.php'; ?>
+<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homepage_dreamescape.css">
+    <title>Lostescaperoom</title>
+</head>
+<body>
+    <header>
+    <img src="../DreamEscapeRoomProject/images/youlose.jpg" alt="youlost" id="backgroundimageyoulost">
+
+    <p id="youlost">You lost! Try again?</p>
+
+    <div id="button1">
+    <a href="room_1.php" id="room1link">Room 1</a>
+    </div>
+
+    <div id="button2">
+    <a href="room_2.php" id="room2link">Room 2</a>
+    </div>
+</header>
+</body>
+<html>
